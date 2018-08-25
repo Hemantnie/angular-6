a1d50a555c2b1c6a5ac6ccf2c2d3706594906dc6
Hemant Kumar <Hemant.Kumar@accolite.com>
2018-08-25 19:38:32 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AccordionModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+import { JokeListComponent } from './joke-list/joke-list.component';
+import { JokeFormComponent } from './joke-form/joke-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    JokeListComponent,
+    JokeFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/joke/joke.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Joke } from '../joke';
+
+
+@Component({
+  selector: 'app-joke',
+  templateUrl: './joke.component.html',
+  styleUrls: ['./joke.component.scss']
+})
+export class JokeComponent implements OnInit {
+
+  @Input('joke')data: Joke;
+  constructor() {
+  }
+  ngOnInit() {
+  }
+
+}
